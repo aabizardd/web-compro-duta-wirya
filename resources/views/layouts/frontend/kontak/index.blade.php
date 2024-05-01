744eb4331c18f26ecaf65e276ccc4548ad0bea26
Muhammad Abizard <abizard@student.telkomuniversity.ac.id>
2024-05-01 10:10:04 +0700

    ok

--- resources/views/layouts/frontend/kontak/index.blade.php
+++ resources/views/layouts/frontend/kontak/index.blade.php
@@ -1,101 +1,107 @@
 @extends('layouts.frontend.layout')
 
 @section('content')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
 
-<div class="breatcome_area d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="breatcome_title">
-                    <div class="breatcome_title_inner pb-2">
-                        <h2>Kontak Kami</h2>
-                    </div>
-                    <div class="breatcome_content">
-                        <ul>
-                            <li><a href="/">Home</a> <i class="fa fa-angle-right"></i> <a href="{{ url('kontak') }}">
-                                    Kontak Kami</a>
-                            </li>
-                        </ul>
+    <div class="breatcome_area d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breatcome_title">
+                        <div class="breatcome_title_inner pb-2">
+                            <h2>Kontak Kami</h2>
+                        </div>
+                        <div class="breatcome_content">
+                            <ul>
+                                <li><a href="/">Home</a> <i class="fa fa-angle-right"></i> <a
+                                        href="{{ url('kontak') }}">
+                                        Kontak Kami</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="main_contact_area style_three pt-80 pb-90">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="section_title text_left mb-50 mt-3">
-                    <div class="section_sub_title uppercase mb-3">
-                        <h6>Kontak Info</h6>
+    <div class="main_contact_area style_three pt-80 pb-90">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="section_title text_left mb-50 mt-3">
+                        <div class="section_sub_title uppercase mb-3">
+                            <h6>Kontak Info</h6>
+                        </div>
+                        <div class="section_main_title">
+                            <h1>Keep In Touch</h1>
+                        </div>
+                        <div class="section_title_text pt-2">
+                            <p>Anda juga dapat mengirimkan email kepada kami, dengan mengisi formulir di samping ini
+                            </p>
+                        </div>
+                        <div class="em_bar">
+                            <div class="em_bar_bg"></div>
+                        </div>
                     </div>
-                    <div class="section_main_title">
-                        <h1>Keep In Touch</h1>
-                    </div>
-                    <div class="section_title_text pt-2">
-                        <p>Anda juga dapat mengirimkan email kepada kami, dengan mengisi formulir di samping ini
-                        </p>
-                    </div>
-                    <div class="em_bar">
-                        <div class="em_bar_bg"></div>
+                    <div class="contact_address">
+                        <div class="contact_address_company mb-3">
+                            <ul>
+                                <li><i class="fa fa-envelope-o"></i><span><a href="#">duta_wirya@yahoo.com</a></span>
+                                </li>
+                                <li><i class="fa fa-whatsapp"></i>
+                                    <span>
+                                        <a href="https://api.whatsapp.com/send?phone=62811875715" target="_blank">
+                                            0811875715</a>
+
+                                    </span>
+                                </li>
+                                <li><i class="fa fa-map-marker"></i> <span> Jl. Ki Mangun
+                                        Sarkoro No. 55. Surakarta 57135</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div class="contact_address">
-                    <div class="contact_address_company mb-3">
-                        <ul>
-                            <li><i class="fa fa-envelope-o"></i><span><a href="#">duta_wirya@yahoo.com</a></span>
-                            </li>
-                            <li><i class="fa fa-mobile"></i><span> +628 13213222233</span></li>
-                            <li><i class="fa fa-map-marker"></i> <span> Jl. Ki Mangun
-                                    Sarkoro No. 55. Surakarta 57135</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="contact_from">
+                <div class="col-lg-6">
+                    <div class="contact_from">
 
 
 
-                    <div class="contact_from_box">
+                        <div class="contact_from_box">
 
-                        @if (Session::has('success'))
-                        <div class="alert alert-success alert-dismissible fade show mb-3" role="alert">
-                            {{ Session::get('success') }}
-                            <button type="button" class="btn btn-sm" data-dismiss="alert" aria-label="Close">
-                                <i class="fa fa-close"></i>
-                            </button>
-                        </div>
-                        @endif
+                            @if (Session::has('success'))
+                                <div class="alert alert-success alert-dismissible fade show mb-3" role="alert">
+                                    {{ Session::get('success') }}
+                                    <button type="button" class="btn btn-sm" data-dismiss="alert" aria-label="Close">
+                                        <i class="fa fa-close"></i>
+                                    </button>
+                                </div>
+                            @endif
 
 
 
-                        <div class="contact_title pb-4">
-                            <h3>Kirim Pesan </h3>
-                        </div>
-                        <form id="contact_form" action="{{ route('kontak') }}" method="POST" id="dreamit-form">
-                            @csrf
+                            <div class="contact_title pb-4">
+                                <h3>Kirim Pesan </h3>
+                            </div>
+
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="form_box mb-30">
-                                        <input type="text" name="nama_pengirim" placeholder="Nama">
+                                        <input type="text" name="nama_pengirim" placeholder="Nama" id="nama_pengirim">
                                     </div>
                                 </div>
 
                                 <div class="col-lg-12">
                                     <div class="form_box mb-30">
-                                        <input type="number" name="no_telepon" placeholder="Nomor Telepon">
+                                        <input type="number" name="no_telepon" placeholder="Nomor Telepon" id="no_telepon">
                                     </div>
                                 </div>
 
 
                                 <div class="col-lg-12">
                                     <div class="form_box mb-30">
-                                        <input type="email" name="from_mail" placeholder="Alamat Email">
+                                        <input type="email" name="from_mail" placeholder="Alamat Email" id="from_mail">
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
@@ -107,81 +113,51 @@
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                        <div class="status"></div>
+
+                            <div class="status"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="google_map_area">
-    <div class="row-fluid">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="google_map_area">
-                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0008792249173!2d106.8491768749907!3d-6.263612793725021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f24e6ad042f5%3A0x6ca614dd38944ef3!2sPT%20Duta%20Wirya!5e0!3m2!1sid!2sid!4v1699095441273!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="google_map_area">
+        <div class="row-fluid">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="google_map_area">
+                    <iframe class="map"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0008792249173!2d106.8491768749907!3d-6.263612793725021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f24e6ad042f5%3A0x6ca614dd38944ef3!2sPT%20Duta%20Wirya!5e0!3m2!1sid!2sid!4v1699095441273!5m2!1sid!2sid"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<script>
-    $(document).ready(function() {
-        $("#kirimPesanBtn").click(function() {
-            // Mengganti teks tombol dengan teks "Loading..."
 
-            var form = document.getElementById("contact_form");
 
-            $(this).html("Loading...");
-            // Menonaktifkan tombol agar tidak dapat ditekan lagi
-            $(this).attr("disabled", true);
+    <script>
+        $(document).ready(function() {
+            $('#kirimPesanBtn').click(function() {
+                var nama = $('#nama_pengirim').val();
+                var telepon = $('#no_telepon').val();
+                var email = $('#from_mail').val();
+                var pesan = $('#message').val();
 
-            form.submit();
+                // Format pesan untuk dikirim ke WhatsApp
+                var waMessage = "Nama: " + nama + "\n" +
+                    "Nomor Telepon: " + telepon + "\n" +
+                    "Email: " + email + "\n" +
+                    "Pesan: " + pesan;
 
+                // Buka tautan WhatsApp dalam tab atau jendela baru
+                window.open("https://wa.me/62811875715?text=" + encodeURIComponent(waMessage), "_blank");
+            });
         });
-    });
-</script>
+    </script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@include('layouts.frontend.jasa.jasa-carousel')
-
+    @include('layouts.frontend.jasa.jasa-carousel')
 @endsection
--- resources/views/layouts/frontend/layout.blade.php
+++ resources/views/layouts/frontend/layout.blade.php
@@ -9,35 +9,45 @@
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="56x56" href="{{ asset('/') }}assets/admin/assets/img/logo dwpc bulat.png" />
+    <link rel="icon" type="image/png" sizes="56x56"
+        href="{{ asset('/') }}assets/admin/assets/img/logo dwpc bulat.png" />
     <!-- bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/bootstrap.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/bootstrap.min.css" type="text/css"
+        media="all" />
     <!-- carousel CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/owl.carousel.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/owl.carousel.min.css" type="text/css"
+        media="all" />
     <!-- nivo-slider CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/nivo-slider.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/nivo-slider.css" type="text/css"
+        media="all" />
     <!-- animate CSS -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/animate.css" type="text/css" media="all" />
     <!-- animated-text CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/animated-text.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/animated-text.css" type="text/css"
+        media="all" />
     <!-- font-awesome CSS -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/fonts/font-awesome/css/font-awesome.min.css" />
+    <link type="text/css" rel="stylesheet"
+        href="{{ asset('/') }}assets/fe/assets/fonts/font-awesome/css/font-awesome.min.css" />
     <!-- font-flaticon CSS -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/flaticon.css" type="text/css" media="all" />
     <!-- theme-default CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/theme-default.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/theme-default.css" type="text/css"
+        media="all" />
     <!-- meanmenu CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/meanmenu.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/meanmenu.min.css" type="text/css"
+        media="all" />
     <!-- Main Style CSS -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/fe/style.css" type="text/css" media="all" />
     <!-- transitions CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/owl.transitions.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/owl.transitions.css" type="text/css"
+        media="all" />
     <!-- venobox CSS -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/fe/venobox/venobox.css" type="text/css" media="all" />
     <!-- widget CSS -->
     <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/widget.css" type="text/css" media="all" />
     <!-- responsive CSS -->
-    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/responsive.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('/') }}assets/fe/assets/css/responsive.css" type="text/css"
+        media="all" />
     <!-- modernizr js -->
     <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/vendor/modernizr-3.5.0.min.js"></script>
 
@@ -55,6 +65,8 @@
                             <ul>
                                 <li><a href="#"><i class="fa fa-clock-o"></i>Senin-Jumat 09.00-16.00</a></li>
                                 <li><a href="#"><i class="fa fa-phone"></i>(0271) 717910</a></li>
+                                <li><a href="https://api.whatsapp.com/send?phone=62811875715" target="_blank"><i
+                                            class="fa fa-whatsapp"></i>0811875715</a></li>
                                 <li><a href="#"><i class="fa fa-envelope-o"></i>duta_wirya@yahoo.com</a></li>
 
 
@@ -87,13 +99,13 @@
 
     @yield('content')
     <?php
-
+    
     use App\Models\Jasa;
     use App\Models\Sejarah;
-
+    
     $jasa = Jasa::all();
     $sejarah = Sejarah::first();
-
+    
     ?>
 
     <div class="footer-middle pt-95">
@@ -102,7 +114,8 @@
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="widget widgets-company-info">
                         <div class="footer-bottom-logo pb-40">
-                            <img src="{{ asset('/') }}assets/admin/assets/img/logo dwpc bulat.png" alt="" width="100" />
+                            <img src="{{ asset('/') }}assets/admin/assets/img/logo dwpc bulat.png" alt=""
+                                width="100" />
                         </div>
                         <div class="company-info-desc">
                             <p style="text-align: justify;">
@@ -121,93 +134,106 @@
                         <div class="menu-quick-link-container ml-4">
                             <ul id="menu-quick-link" class="menu">
                                 @foreach ($jasa as $key => $item)
-                                @if ($key < 5) <li><a href="{{ url('jasa') ."?jasa=". $item->id }}">{{ $item->nama_jasa }}</a></li>
+                                    @if ($key < 5)
+                                        <li><a
+                                                href="{{ url('jasa') . '?jasa=' . $item->id }}">{{ $item->nama_jasa }}</a>
+                                        </li>
                                     @else
                                     @break
-                                    @endif
-                                    @endforeach
+                                @endif
+                            @endforeach
 
-                                    <li><a href="{{ url('jasa') }}">Selengkapnya <i class="fa fa-long-arrow-right"></i></a></li>
+                            <li><a href="{{ url('jasa') }}">Selengkapnya <i
+                                        class="fa fa-long-arrow-right"></i></a></li>
 
-                            </ul>
-                        </div>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="widget widgets-company-info">
-                        <h3 class="widget-title pb-4">Alamat Perusahaan</h3>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="widget widgets-company-info">
+                    <h3 class="widget-title pb-4">Alamat Perusahaan</h3>
 
-                        <div class="footer-social-info">
-                            <p><i class="fa fa-street-view"></i> <span>Address :</span>“Wisma Penilai” lantai 4-5, Jl.
-                                Ki Mangunsarkoro No. 55, Nusukan, kota Surakarta, 57135</p>
-                        </div>
-                        <div class="footer-social-info">
-                            <p><i class="fa fa-phone"></i> <span>Phone :</span> (0271) 717910.</p>
-                        </div>
-                        <div class="footer-social-info">
-                            <p><i class="fa fa-envelope"></i> <span>Email :</span>duta_wirya@yahoo.com</p>
-                        </div>
-
+                    <div class="footer-social-info">
+                        <p><i class="fa fa-street-view"></i> <span>Address :</span>“Wisma Penilai” lantai 4-5, Jl.
+                            Ki Mangunsarkoro No. 55, Nusukan, kota Surakarta, 57135</p>
                     </div>
-                </div>
+                    <div class="footer-social-info">
+                        <p><i class="fa fa-phone"></i> <span>Phone :</span> (0271) 717910.</p>
+                    </div>
 
+                    <div class="footer-social-info">
+                        <p><i class="fa fa-whatsapp"></i> <span>Whatsapp :</span>
+                            <a href="https://api.whatsapp.com/send?phone=62811875715" target="_blank"
+                                class="text-white">
+                                0811875715</a>
+                        </p>
+                    </div>
+
+                    <div class="footer-social-info">
+                        <p><i class="fa fa-envelope"></i> <span>Email :</span>duta_wirya@yahoo.com</p>
+                    </div>
+
+                </div>
             </div>
 
         </div>
-        <div class="row footer-bottom mt-70 pt-3 pb-1">
-            <div class="col-lg-6 col-md-6">
-                <div class="footer-bottom-content">
-                    <div class="footer-bottom-content-copy">
-                        <p>© {{ date('Y') }} PT Duta Wirya. </p>
-                    </div>
+
+    </div>
+    <div class="row footer-bottom mt-70 pt-3 pb-1">
+        <div class="col-lg-6 col-md-6">
+            <div class="footer-bottom-content">
+                <div class="footer-bottom-content-copy">
+                    <p>© {{ date('Y') }} PT Duta Wirya. </p>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6">
-                <div class="footer-bottom-right">
-                    <div class="footer-bottom-right-text">
-                        <a class="absod" href="#">Privacy Policy </a>
-                        <a href="#"> Terms & Conditions</a>
-                    </div>
+        </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="footer-bottom-right">
+                <div class="footer-bottom-right-text">
+                    <a class="absod" href="#">Privacy Policy </a>
+                    <a href="#"> Terms & Conditions</a>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-    <!-- jquery js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/vendor/jquery-3.2.1.min.js"></script>
-    <!-- bootstrap js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/bootstrap.min.js"></script>
-    <!-- carousel js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/owl.carousel.min.js"></script>
-    <!-- counterup js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.counterup.min.js"></script>
-    <!-- waypoints js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/waypoints.min.js"></script>
-    <!-- wow js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/wow.js"></script>
-    <!-- imagesloaded js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/imagesloaded.pkgd.min.js"></script>
-    <!-- venobox js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/venobox/venobox.js"></script>
-    <!-- ajax mail js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/ajax-mail.js"></script>
-    <!--  testimonial js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/testimonial.js"></script>
-    <!--  animated-text js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/animated-text.js"></script>
-    <!-- venobox min js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/venobox/venobox.min.js"></script>
-    <!-- isotope js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/isotope.pkgd.min.js"></script>
-    <!-- jquery nivo slider pack js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.nivo.slider.pack.js"></script>
-    <!-- jquery meanmenu js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.meanmenu.js"></script>
-    <!-- jquery scrollup js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.scrollUp.js"></script>
-    <!-- theme js -->
-    <script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/theme.js"></script>
-    <!-- jquery js -->
+</div>
+</div>
+<!-- jquery js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/vendor/jquery-3.2.1.min.js"></script>
+<!-- bootstrap js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/bootstrap.min.js"></script>
+<!-- carousel js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/owl.carousel.min.js"></script>
+<!-- counterup js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.counterup.min.js"></script>
+<!-- waypoints js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/waypoints.min.js"></script>
+<!-- wow js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/wow.js"></script>
+<!-- imagesloaded js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/imagesloaded.pkgd.min.js"></script>
+<!-- venobox js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/venobox/venobox.js"></script>
+<!-- ajax mail js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/ajax-mail.js"></script>
+<!--  testimonial js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/testimonial.js"></script>
+<!--  animated-text js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/animated-text.js"></script>
+<!-- venobox min js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/venobox/venobox.min.js"></script>
+<!-- isotope js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/isotope.pkgd.min.js"></script>
+<!-- jquery nivo slider pack js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.nivo.slider.pack.js"></script>
+<!-- jquery meanmenu js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.meanmenu.js"></script>
+<!-- jquery scrollup js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/jquery.scrollUp.js"></script>
+<!-- theme js -->
+<script type="text/javascript" src="{{ asset('/') }}assets/fe/assets/js/theme.js"></script>
+<!-- jquery js -->
 </body>
 
 
